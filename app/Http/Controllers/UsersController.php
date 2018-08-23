bb51e72392cb965f2ff4f4f4cde73d55836b12b1
AubreyV <breyvicente@gmail.com>
2018-08-23 18:28:13 +0800

    edit user profile implementation

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -62,7 +62,7 @@ class UsersController extends Controller
     public function edit(User $user)
     {
 
-        
+        return view('users.edit', compact('user'));
     }
 
     /**
@@ -75,7 +75,23 @@ class UsersController extends Controller
     public function update(Request $request, User $user)
     {
 
+        $user->fill([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        if($request->hasFile('avatar')) {
+            $avatar = $request->file('avatar');
+            $filename = time() . '.' . $avatar->getClientOriginalExtension();
+            Image::make($avatar)->resize(300,300)->save( public_path('/uploads/avatars/' . $filename) );
+
+            $user->avatar = $filename;
+        }
         
+        $user->save();
+
+        return redirect()->route('users.show', compact('user'));
     }
 
     /**
