4fe00636efe9ea31af849df3462317952dba5da9
AubreyV <breyvicente@gmail.com>
2018-08-29 12:15:11 +0800

    display all categories (paginated)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,15 +41,14 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-
-                        <li><a href="#">Categories</a></li>
-                        <li><a href="{{ route('users.index') }}">Members</a></li>
-
+                        
                         <!-- Authentication Linkhs -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li><a href=" {{ route('categories.index') }}">Categories</a></li>
+                            <li><a href="{{ route('users.index') }}">Members</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     <img src="/uploads/avatars/{{ Auth::user()->avatar }}" style="width:20px;height:20px;"> {{ Auth::user()->name }} <span class="caret"></span>
