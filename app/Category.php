50370a4643e9471698162b5846799bdae5595c01
AubreyV <breyvicente@gmail.com>
2018-08-29 17:48:49 +0800

    render categories in desc order of time of creation

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function scopeOrder($query)
+    {
+    	return $query->orderBy('created_at', 'desc');
+    }
 }
