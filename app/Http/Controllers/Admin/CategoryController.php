55e227f3aee871efa683a374c64098be60f7a547
AubreyV <breyvicente@gmail.com>
2018-09-04 10:51:36 +0800

    added delete a category feature

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -103,6 +103,8 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return redirect()->route('admin.categories.index');
     }
 }
